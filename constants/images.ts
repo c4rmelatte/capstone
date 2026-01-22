a8e97d2a58198cac2665ba7ad98439cea27316b7
carms <carmsberbs@gmail.com>
2026-01-22 21:43:05 +0800

    Update images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -8,8 +8,29 @@ import Slide1 from "../assets/images/slide1.png";
 import Slide2 from "../assets/images/slide2.png";
 import Slide3 from "../assets/images/slide3.png";
 import Loginbg from "../assets/images/bglogin.png";
+<<<<<<< Updated upstream
 import Check from "../assets/images/checked.png";
 
 
 const Images = { Slide1, Slide2, Slide3, Image_BG, Item1, Item2, Item3, Item4, Banner, Loginbg, Check };
+=======
+import Puset from "../assets/images/puset.png";
+import StubyLogo from "../assets/images/stubylogo.png";
+import YellowWaveIntro from "../assets/images/YellowWaveIntro.png";
+import FlashcardWaveBg from "../assets/images/flashcardWaveBg.png";
+import MusicWaveBg from "../assets/images/musicWaveBg.png";
+import PomodoroWaveBg from "../assets/images/pomodoroWaveBg.png";
+import ToDoWaveBg from "../assets/images/toDoWaveBg.png";
+import NotesWaveBg from "../assets/images/notesWaveBg.png";
+import FlashLearn from "../assets/images/flashLearn.png";
+import TimeFocus from "../assets/images/timeFocus.png";
+import TaskReady from "../assets/images/taskReady.png";
+import FocusBeats from "../assets/images/focusBeats.png";
+import QuickNotes from "../assets/images/quickNotes.png";
+import Check from "../assets/images/checked.png";
+
+
+const Images = {FlashLearn, Check, TimeFocus, TaskReady, FocusBeats, QuickNotes, FlashcardWaveBg, 
+    MusicWaveBg, PomodoroWaveBg, NotesWaveBg, ToDoWaveBg, YellowWaveIntro, Slide1, Slide2, Slide3, Image_BG, Item1, Item2, Item3, Item4, Banner, Loginbg, Puset, StubyLogo };
+>>>>>>> Stashed changes
 export default Images;
