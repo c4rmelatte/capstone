283c82fd177b635d33c64d7c2d49d4c4c8e86a8c
ncarmela <ninacarmela01@gmail.com>
2026-01-26 10:55:44 +0800

    wait

---
+++ app/(tabs)/flashcard/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function FlashCardLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="product" />
+    </Stack>
+  );
+}
