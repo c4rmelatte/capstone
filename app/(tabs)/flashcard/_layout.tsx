37d10e7c204d4fbfa838858b62f1c91497ef0592
ncarmela <ninacarmela01@gmail.com>
2026-01-26 11:09:42 +0800

    flashcard

--- app/(tabs)/flashcard/_layout.tsx
+++ app/(tabs)/flashcard/_layout.tsx
@@ -5,6 +5,7 @@ export default function FlashCardLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="product" />
+      
     </Stack>
   );
 }
--- app/(tabs)/flashcard/index.tsx
+++ app/(tabs)/flashcard/index.tsx
@@ -1,37 +1,31 @@
-// import React from "react";
-// import { View, Text, Dimensions } from "react-native";
+import React from "react";
+import { View, Text, Dimensions } from "react-native";
+import AddFloatingButton from "@/components/AddFloatingButton";
 
-// const { width } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 
-// const FlashCard = () => {
-//   return (
-//     <View className="flex-1 items-center mt-20">
-//       <Text className="text-2xl font-bold mb-6">FlashCard Screen</Text>
-//       <View
-//         className="overflow-hidden rounded-2xl shadow-md"
-//         style={{ width: width * 0.9, height: 180 }}
-//       >
-//         {/* Top 20% */}
-//         <View className="flex-[2] bg-[#80CF8F]" />
+const FlashCard = () => {
+  return (
+    <View className="flex-1 items-center mt-20">
+      <Text className="text-2xl font-bold mb-6">FlashCard Screen</Text>
+      <View
+        className="overflow-hidden rounded-2xl shadow-md"
+        style={{ width: width * 0.9, height: 180 }}
+      >
+        {/* Top 20% */}
+        <View className="flex-[2] bg-[#39675F]" />
 
-//         {/* Bottom 80% */}
-//         <View className="flex-[8] bg-[#FFF9E5] items-center justify-center px-4">
-//           <Text className="text-base text-black">FlashCard Content</Text>
-//         </View>
-//       </View>
+        {/* Bottom 80% */}
+        <View className="flex-[8] bg-[#FFF9E5] items-center justify-center px-4">
+          <Text className="text-base text-black">FlashCard Content</Text>
+        </View>
+      </View>
 
-//         <TouchableOpacity
-//         onPress={addTodoItem}
-//         activeOpacity={0.85}
-//         className="absolute bottom-6 right-6 sm:bottom-10 sm:right-10
-//                    w-16 h-16 rounded-full bg-[#F5CE8E]
-//                    items-center justify-center shadow-lg"
-//       >
-//         <Plus size={32} color="black" />
-//       </TouchableOpacity>
-      
-//     </View>
-//   );
-// };
 
-// export default FlashCard;
+<AddFloatingButton onPress={() => {}} />
+
+    </View>
+  );
+};
+
+export default FlashCard;
