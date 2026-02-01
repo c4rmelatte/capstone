0ca6aa6d1fe6cf99ec9ce33159247406dcb44b54
ncarmela <ninacarmela01@gmail.com>
2026-02-01 10:06:25 +0800

    fixed

--- app/(tabs)/todo/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function ToDoLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
